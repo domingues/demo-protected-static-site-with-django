37b992084fd1819f4d0dc9ce75ce830b2a7528f4
Fábio Domingues <fabio.a.domingues@gmail.com>
2024-01-27 21:51:15 +0000

    fix: allow access to the admin panel with "/admin"

--- domingues/protected_static_site/urls.py
+++ domingues/protected_static_site/urls.py
@@ -9,10 +9,12 @@ import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.shortcuts import redirect
+from django.urls import include, path, reverse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("admin", lambda _: redirect(reverse("admin:index"))),
     path("__debug__/", include(debug_toolbar.urls)),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
